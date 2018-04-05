e3e3462a9873c5adb8f46b1be5d92565c1d6a29e
andrewbackes <rbackes@ucla.edu>
2018-04-05 16:49:23 -0700

    Limit fetch to just symlink default configs

--- src/config/default/fetch.sh
+++ src/config/default/fetch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+# Setup the default configuration that comes with a distribution
 for filename in /opt/spinnaker/config/default/*.yml; do
     ln -s $filename /opt/spinnaker/config/
 done
