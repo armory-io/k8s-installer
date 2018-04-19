b4d54092a120c6869b360b733a97d2aa524654ff
Sandeep Raju <srand48@gmail.com>
2018-04-19 14:01:30 -0700

    prompt user for all certs and use them to create the service account

--- src/add-acct.sh
+++ src/add-acct.sh
@@ -7,35 +7,55 @@ fi
 
 export NAMESPACE=${NAMESPACE:-armory}
 export KUBECTL_OPTIONS="--namespace=${NAMESPACE}"
+export LIGHTHOUSE_URL="http://spinnaker.dev.armory.io:5000"
 
 function error() {
   >&2 echo $1
   exit 1
 }
 
-function prompt_user_for_certs() {
-  echo -n "Enter path to $1: "
-  read ca_cert
-
-  openssl rsa -in "$ca_cert" -check -noout
-  if [ "$?" != 0 ]; then
-    error "$1 cert not valid"
+function get_var() {
+  local text=$1
+  local var_name="${2}"
+  local val_func=${3}
+  local val_list=${4}
+  local default_val="${5}"
+  if [ -z ${!var_name} ]; then
+    [ ! -z "$val_list" ] && $val_list
+    echo -n "${text}"
+    read value
+    if [ -z "${value}" ]; then
+      echo "Not using ${var_name}"
+    elif [ ! -z "$val_func" ] && ! $val_func ${value}; then
+      get_var "$1" $2 $3
+    else
+      export ${var_name}=${value}
+    fi
   fi
-  export CERT_PATH="$ca_cert"
 }
 
 function create_service_acct() {
   export SERVICE_ACCOUNT_NAME="$(mktemp -u armory-svc-acct-XXXXXXXXXXXX | tr '[:upper:]' '[:lower:]')"
-  #prompt_user_for_certs "CA cert for the K8s cluster"
 
-  if [ ! -z "$CERT_PATH" ]; then
+  get_var "Enter path to cert file for the certificate authority: " CA_CERT
+  if [ ! -z "$CA_CERT" ]; then
     KUBECTL_OPTIONS="--certificate-authority=$CERT_PATH $KUBECTL_OPTIONS"
   fi
+  get_var "Enter path to client certificate file for TLS: " CLIENT_CERT
+  if [ ! -z "$CLIENT_KEY" ]; then
+    KUBECTL_OPTIONS="--client-key=$CLIENT_CERT $KUBECTL_OPTIONS"
+  fi
+    get_var "Enter path to client key file for TLS: " CLIENT_KEY
+  if [ ! -z "$CLIENT_KEY" ]; then
+    KUBECTL_OPTIONS="--client-key=$CLIENT_KEY $KUBECTL_OPTIONS"
+  fi
+
   kubectl ${KUBECTL_OPTIONS} create serviceaccount "$SERVICE_ACCOUNT_NAME"
 }
 
 function generate_kubeconfig() {
-  ./create-kubeconfig "$SERVICE_ACCOUNT_NAME" "$KUBECTL_OPTIONS"
+  export KUBE_CONF="$(mktemp -u kubeconf-XXXXXXX)"  # DO NOT call this var KUBECONFIG!!
+  ./create-kubeconfig "$SERVICE_ACCOUNT_NAME" "$KUBECTL_OPTIONS" > "$KUBE_CONF"
 }
 
 function main() {
@@ -50,6 +70,8 @@ function main() {
 EOF
   create_service_acct
   generate_kubeconfig
+  curl -X POST "$LIGHTHOUSE_URL"/v1/configs/accounts/kubernetes -d @"$KUBE_CONF"
+  rm -rf "$KUBE_CONF"
 }
 
 main
