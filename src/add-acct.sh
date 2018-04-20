9e5b5bd9febfdf7c33873dd64c2dc7a80651002a
Sandeep Raju <srand48@gmail.com>
2018-04-20 16:44:46 -0700

    Eng 1720 gen kubeconfig (#64)
    * initial version
    
    * prompt user for all certs and use them to create the service account
    
    * fix indentation
    
    * Prompt for spinnaker url
    
    * fixed kubectl option
    
    * use gate url
    
    * more typos & fixes
    
    * use certs to talk to k8s api host for generating kubeconfig
    
    * mege damage
    
    * changed JSON to match endpoint
    
    * removed debug
    
    * update lighthouse, post correct json data

--- src/add-acct.sh
+++ src/add-acct.sh
@@ -39,29 +39,45 @@ function create_service_acct() {
 
   get_var "Enter path to cert file for the certificate authority: " CA_CERT
   if [ ! -z "$CA_CERT" ]; then
-    KUBECTL_OPTIONS="--certificate-authority=$CERT_PATH $KUBECTL_OPTIONS"
+    export KUBECTL_OPTIONS="--certificate-authority=$CA_CERT $KUBECTL_OPTIONS"
   fi
   get_var "Enter path to client certificate file for TLS: " CLIENT_CERT
   if [ ! -z "$CLIENT_CERT" ]; then
-    KUBECTL_OPTIONS="--client-certificate=$CLIENT_CERT $KUBECTL_OPTIONS"
+    export KUBECTL_OPTIONS="--client-certificate=$CLIENT_CERT $KUBECTL_OPTIONS"
   fi
   get_var "Enter path to client key file for TLS: " CLIENT_KEY
   if [ ! -z "$CLIENT_KEY" ]; then
-    KUBECTL_OPTIONS="--client-key=$CLIENT_KEY $KUBECTL_OPTIONS"
+    export KUBECTL_OPTIONS="--client-key=$CLIENT_KEY $KUBECTL_OPTIONS"
+  fi
+  get_var "Enter K8s API/master hostname or IP (eg: https://146.148.69.252): " SERVER_NAME
+  if [ ! -z "$SERVER_NAME" ]; then
+    export KUBECTL_OPTIONS="--server=$SERVER_NAME $KUBECTL_OPTIONS"
   fi
 
   kubectl ${KUBECTL_OPTIONS} create serviceaccount "$SERVICE_ACCOUNT_NAME"
 }
 
 function generate_kubeconfig() {
-  export KUBE_CONF="$(mktemp -u kubeconf-XXXXXXX)"  # DO NOT call this var KUBECONFIG!!
-  ./create-kubeconfig "$SERVICE_ACCOUNT_NAME" "$KUBECTL_OPTIONS" > "$KUBE_CONF"
+  export B64_KUBE_CONF="$(mktemp -u kubeconf-XXXXXXX)"
+  get_var "Enter the K8s context you want to use: " CONTEXT
+  get_var "Enter the K8s cluster you want to use: " CLUSTER
+  if [ ! -z "$CONTEXT" ]; then
+    ./create-kubeconfig "$SERVICE_ACCOUNT_NAME" "$CONTEXT" "$CLUSTER" "$KUBECTL_OPTIONS" | base64 > "$B64_KUBE_CONF"
+  fi
 }
 
 function post_kubeconfig_to_lighthouse() {
   get_var "Enter gate URL for your spinnaker install (eg: http://spinnaker.company.com:8084): " GATE_URL
-  curl -X POST "$GATE_URL"/armory/v1/configs/accounts/kubernetes -d @"$KUBE_CONF"
-  rm -rf "$KUBE_CONF"
+
+  DATA="{
+    \"kubeconfig\": \"$(cat $B64_KUBE_CONF)\",
+    \"namespaces\": [\"$NAMESPACE\"],
+    \"account_name\": \"$CLUSTER\",
+    \"is_service_account\": true
+  }"
+
+  curl -X POST -H "Content-Type: application/json" "$GATE_URL"/armory/v1/configs/accounts/kubernetes -d "$DATA"
+  rm -rf "$B64_KUBE_CONF"
 }
 
 function main() {
