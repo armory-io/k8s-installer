b1e051c12bc71cf8288a1f8dbbf1c5c151f435fb
Isaac Mosquera <imosquer@gmail.com>
2018-04-24 16:55:29 -0700

    ca certs are now supported through the k8s installer

--- src/config/default/fetch.sh
+++ src/config/default/fetch.sh
@@ -11,3 +11,18 @@ done
 for filename in /opt/spinnaker/config/custom/*.yml; do
     cp $filename /opt/spinnaker/config/
 done
+
+# if CA exists, mount it into the default JKS store
+ca_cert_path="/opt/spinnaker/certs/ca.crt"
+jks_path="/etc/ssl/certs/java/cacerts"
+if [  -f ${ca_cert_path} ]; then
+    echo "Loading CA cert into the Java Keystore located at ${jks_path}"
+    keytool -importcert \
+        -file ${ca_cert_path} \
+        -keystore ${jks_path} \
+        -alias custom-ca \
+        -storepass changeit \
+        -noprompt
+else
+    echo "No CA cert found at ${ca_cert_path}"
+fi
