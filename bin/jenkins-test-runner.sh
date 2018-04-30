7fc4035d02e98f501374c5a283fdd464a0eb0cf9
andrewbackes <rbackes@ucla.edu>
2018-04-30 15:38:35 -0700

    Typo

--- bin/jenkins-test-runner.sh
+++ bin/jenkins-test-runner.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")/.."
 uuid=$(uuidgen || dbus-uuidgen)
-IMAGE=(k8s-installer-test-runner:${uuid}
+IMAGE=k8s-installer-test-runner:${uuid}
 docker build -t $IMAGE -f bin/tester.Dockerfile .
 
 docker run --rm -ti -v /root/:/root/ -v "$(pwd):/k8s-installer" $IMAGE /k8s-installer/integration
