92e626eba1d377bb07fcf2c569c1dfcaba1efec2
andrewbackes <rbackes@ucla.edu>
2018-04-30 16:26:39 -0700

    WIP

--- bin/jenkins-test-runner.sh
+++ bin/jenkins-test-runner.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
 
 cd "$(dirname "$0")/.."
+JENKINS_HOME=${JENKINS_HOME:-/root/}
+
 uuid=$(uuidgen || dbus-uuidgen)
 IMAGE=k8s-installer-test-runner:${uuid}
 docker build -t $IMAGE -f bin/tester.Dockerfile .
 
 docker run --rm \
-    -v /root/.aws/credentials:/root/.aws/credentials \
-    -v /root/.kube/config:/root/.kube/config \
+    -e GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.kube/gcp_key.json" \
+    -v "${JENKINS_HOME}/.aws/credentials:/root/.aws/credentials" \
+    -v "${JENKINS_HOME}/.kube/config:/root/.kube/config" \
+    -v "${JENKINS_HOME}/.kube/gcp_key.json:/root/.kube/gcp_key.json" \
     -v "$(pwd):/k8s-installer/" \
     $IMAGE \
         /k8s-installer/bin/integration
