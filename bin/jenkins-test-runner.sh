267c87475e6affeb0bdad2d792c78487d642df96
andrewbackes <rbackes@ucla.edu>
2018-04-30 15:53:03 -0700

    Try it again

--- bin/jenkins-test-runner.sh
+++ bin/jenkins-test-runner.sh
@@ -5,4 +5,9 @@ uuid=$(uuidgen || dbus-uuidgen)
 IMAGE=k8s-installer-test-runner:${uuid}
 docker build -t $IMAGE -f bin/tester.Dockerfile .
 
-docker run --rm -v /root/:/root/ -v "$(pwd):/k8s-installer/" $IMAGE /k8s-installer/bin/integration
+docker run --rm \
+    -v /root/.aws/credentials:/root/.aws/credentials \
+    -v /root/.kube/config:/root/.kube/config \
+    -v "$(pwd):/k8s-installer/" \
+    $IMAGE \
+        /k8s-installer/bin/integration
