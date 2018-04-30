dee48f8a2458016ee876bb2f4538b1d370fce7fb
andrewbackes <rbackes@ucla.edu>
2018-04-30 12:21:51 -0700

    Bash 3 function style

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
-function generate_json_with_trailing_comma() {
+function generate_json_with_trailing_comma {
   source ../src/version.manifest
   fields=$(cat ../src/version.manifest | cut -d' ' -f2 | cut -d'=' -f1)
 
@@ -12,12 +12,12 @@ function generate_json_with_trailing_comma() {
   echo "}"
 }
 
-function generate_json() {
+function generate_json {
   # remove trailing comma
   generate_json_with_trailing_comma | sed 's/,}/}/g'
 }
 
-function upload_version_info() {
+function upload_version_info {
   generate_json > versions.tmp.json
   aws s3 cp versions.tmp.json s3://armory-web/k8s-latest.json
   rm versions.tmp.json
