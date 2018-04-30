69ae1e98e0131433fc9ef1daa00ea5fd96ffa302
andrewbackes <rbackes@ucla.edu>
2018-04-30 15:40:34 -0700

    Jenkins isnt tty

--- bin/jenkins-test-runner.sh
+++ bin/jenkins-test-runner.sh
@@ -5,4 +5,4 @@ uuid=$(uuidgen || dbus-uuidgen)
 IMAGE=k8s-installer-test-runner:${uuid}
 docker build -t $IMAGE -f bin/tester.Dockerfile .
 
-docker run --rm -ti -v /root/:/root/ -v "$(pwd):/k8s-installer" $IMAGE /k8s-installer/integration
+docker run --rm -v /root/:/root/ -v "$(pwd):/k8s-installer" $IMAGE /k8s-installer/integration
