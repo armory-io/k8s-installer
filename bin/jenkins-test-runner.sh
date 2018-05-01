4e56c2b9c2e361ab40a3052a7af391c39dd2be67
andrewbackes <rbackes@ucla.edu>
2018-05-01 09:29:32 -0700

    Try settings mount to rw

--- bin/jenkins-test-runner.sh
+++ bin/jenkins-test-runner.sh
@@ -9,9 +9,12 @@ docker build -t $IMAGE -f bin/tester.Dockerfile .
 
 docker run --rm \
     -e GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.kube/gcp_key.json" \
-    -v "${JENKINS_HOME}/.aws/credentials:/root/.aws/credentials" \
-    -v "${JENKINS_HOME}/.kube/config:/root/.kube/config" \
-    -v "${JENKINS_HOME}/.kube/gcp_key.json:/root/.kube/gcp_key.json" \
-    -v "$(pwd):/k8s-installer/" \
+    -v "${JENKINS_HOME}/:/root/:rw" \
+    -v "$(pwd):/k8s-installer/:rw" \
     $IMAGE \
         /k8s-installer/bin/integration
+
+
+#    -v "${JENKINS_HOME}/.aws/credentials:/root/.aws/credentials" \
+#    -v "${JENKINS_HOME}/.kube/config:/root/.kube/config" \
+#    -v "${JENKINS_HOME}/.kube/gcp_key.json:/root/.kube/gcp_key.json" \
