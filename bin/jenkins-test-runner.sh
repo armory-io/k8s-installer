1d10da38f92a91c07b1a310d987cdb44105c0fe8
andrewbackes <rbackes@ucla.edu>
2018-05-01 09:38:30 -0700

    I'm gonna get it... my god

--- bin/jenkins-test-runner.sh
+++ bin/jenkins-test-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 cd "$(dirname "$0")/.."
-JENKINS_HOME=${JENKINS_HOME:-/root/}
+JENKINS_USER_HOME=${JENKINS_USER_HOME:-/root/}
 
 uuid=$(uuidgen || dbus-uuidgen)
 IMAGE=k8s-installer-test-runner:${uuid}
@@ -9,13 +9,16 @@ docker build -t $IMAGE -f bin/tester.Dockerfile .
 
 docker run --rm \
     -e GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.kube/gcp_key.json" \
-    -v "${JENKINS_HOME}/.aws/:/root/.aws/" \
-    -v "${JENKINS_HOME}/.kube/:/root/.kube/:rw" \
+    -v "${JENKINS_USER_HOME}/.aws/credentials:/root/.aws/credentials" \
+    -v "${JENKINS_USER_HOME}/.kube/config:/root/.kube/config" \
+    -v "${JENKINS_USER_HOME}/.kube/gcp_key.json:/root/.kube/gcp_key.json" \
     -v "$(pwd):/k8s-installer/:rw" \
     $IMAGE \
         /k8s-installer/bin/integration
 
 
-#    -v "${JENKINS_HOME}/.aws/credentials:/root/.aws/credentials" \
-#    -v "${JENKINS_HOME}/.kube/config:/root/.kube/config" \
-#    -v "${JENKINS_HOME}/.kube/gcp_key.json:/root/.kube/gcp_key.json" \
+#    -v "${JENKINS_USER_HOME}/.aws/credentials:/root/.aws/credentials" \
+#    -v "${JENKINS_USER_HOME}/.kube/config:/root/.kube/config" \
+#    -v "${JENKINS_USER_HOME}/.kube/gcp_key.json:/root/.kube/gcp_key.json" \
+#    -v "${JENKINS_USER_HOME}/.aws/:/root/.aws/" \
+#    -v "${JENKINS_USER_HOME}/.kube/:/root/.kube/:rw" \
