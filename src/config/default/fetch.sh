56f98cdd3136d3d59b614520e383b184874ef102
Paul <dotdotdotpaul@users.noreply.github.com>
2018-05-08 12:47:41 -0700

    ENG-1802 saml config (#118)
    * Sanity check.  Create self-signed key in gate.
    
    ...done with fetch, when flagged appropriately in the deployment command.
    Opening HTTPS ports on the nginx container for when it's actually listening.
    
    * Since we're building our own key, default config
    
    Every deploy will re-gen a new 10,000-day long key, so since this is a fixed
    location, we can default some of these values in the config.
    
    * Sanity check.  Convert pem->pkcs12->jks
    
    Needed to update gate-armory image to newer version, with openssl which allows
    us to generate a pkcs12 file that can be imported into jks with a password.
    
    * Look for the .pem file in saml/saml.pem
    
    See Lighthouse changes for the ability to upload this file.
    
    * Update lighthouse version to allow SAML files
    
    * Facepalm.
    
    * See if this fixes the exception.
    
    * Cannot put SAML config here without breaking stuff
    
    Apparently, even if disabled, Spinnaker attempts to validate all the config
    values and thus fails when it can't find things.
    
    * Comment out HTTPS config for now.
    
    * Aha.  My custom nginx.conf got pulled in.
    
    * Add a templated gate-local.yml for reference.
    
    * Update to match Lighthouse changes:
    
    ref. https://github.com/armory-io/lighthouse/pull/186

--- src/config/default/fetch.sh
+++ src/config/default/fetch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
 CONFIG_LOCATION=${SPINNAKER_HOME:-"/opt/spinnaker"}/config/
+CONTAINER=$1
 
 rm -f /opt/spinnaker/config/*.yml
 
@@ -30,3 +31,32 @@ if [  -f ${ca_cert_path} ]; then
 else
     echo "No CA cert found at ${ca_cert_path}"
 fi
+
+saml_pem_path="/opt/spinnaker/config/custom/saml.pem"
+saml_pkcs12_path="/tmp/saml.pkcs12"
+saml_jks_path="${CONFIG_LOCATION}/saml.jks"
+if [ "${CONTAINER}" == "gate" ]; then
+    if [ -f ${saml_pem_path} ]; then
+        echo "Loading ${saml_pem_path} into ${saml_jks_path}"
+        # Convert PEM to PKCS12 with a password.
+        openssl pkcs12 -export -out ${saml_pkcs12_path} -in ${saml_pem_path} -password pass:changeit -name saml
+        keytool -genkey -v -keystore ${saml_jks_path} -alias saml \
+                -keyalg RSA -keysize 2048 -validity 10000 \
+                -storepass changeit -keypass changeit -dname "CN=armory"
+        keytool -importkeystore \
+                -srckeystore ${saml_pkcs12_path} \
+                -srcstoretype PKCS12 \
+                -srcstorepass changeit \
+                -destkeystore ${saml_jks_path} \
+                -deststoretype JKS \
+                -storepass changeit \
+                -alias saml \
+                -destalias saml \
+                -noprompt
+    else
+        echo "No SAML IDP pemfile found at ${saml_pem_path}"
+    fi
+fi
+
+
+
