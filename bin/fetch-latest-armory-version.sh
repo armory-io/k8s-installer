ad7bc850ef0e2ac54460914993ce5632cbaa0174
Kevin Woo <kevinawoo@gmail.com>
2018-05-09 13:02:27 -0700

    fixed variable naming

--- bin/fetch-latest-armory-version.sh
+++ bin/fetch-latest-armory-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 cd "$(dirname "$0")"
 
-ARMORY_JENKINS_JOB_ID=${ARMORY_JENKINS_JOB_ID:-"lastSuccessfulBuild"}
+PUBLIC_ARMORY_JENKINS_JOB_VERSION=${PUBLIC_ARMORY_JENKINS_JOB_VERSION:-"lastSuccessfulBuild"}
 
-echo "Querying ArmorySpinnaker's Jenkins Job for '${ARMORY_JENKINS_JOB_ID}'"
-arm jenkins "/job/armory/job/armoryspinnaker/job/master/${ARMORY_JENKINS_JOB_ID}/artifact/src/spinnaker/version.manifest" > ../src/version.manifest
+echo "Querying ArmorySpinnaker's Jenkins Job for '${PUBLIC_ARMORY_JENKINS_JOB_VERSION}'"
+arm jenkins "/job/armory/job/armoryspinnaker/job/master/${PUBLIC_ARMORY_JENKINS_JOB_VERSION}/artifact/src/spinnaker/version.manifest" > ../src/version.manifest
