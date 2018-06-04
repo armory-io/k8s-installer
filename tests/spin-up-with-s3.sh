ee902d8f0e7164d7a8b1130d0161e39a06442c9e
Sandeep Raju <srand48@gmail.com>
2018-06-04 11:44:02 -0700

    move rb options to the end

--- tests/spin-up-with-s3.sh
+++ tests/spin-up-with-s3.sh
@@ -45,6 +45,6 @@ set -e
 
 # cleanup
 kubectl delete ns $NAMESPACE
-aws --profile "${AWS_PROFILE}" --region us-east-1 s3 rb --force "s3://${ARMORY_CONF_STORE_BUCKET}"
+aws --profile "${AWS_PROFILE}" --region us-east-1 s3 rb "s3://${ARMORY_CONF_STORE_BUCKET}" --force
 
 exit $EXIT_CODE
